285ff599a8b20c1782e14cbe074251be37dee022
sergethi <nsergearistide@gmail.com>
2022-01-28 20:27:53 +0000

    completed header js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import react from 'react';
+import './Header.css'
 
 const Header = () => {
 
     return (
-        <h1>This is Header file</h1>
+        <nav>
+            <h1> <a href="#">K9friends  </a> </h1>
+            <ul>
+                <li><a href="#">Find A Pet</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Log in</a></li>
+            </ul>
+        </nav>
+        // <h1>This is Header file</h1>
     )
 }
 
