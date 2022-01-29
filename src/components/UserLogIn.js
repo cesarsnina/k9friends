b890166ec9f91e61407357f9f59a66fedee57bc2
cesarsnina <cesar.nina05@gmail.com>
2022-01-29 02:22:10 +0000

    added button to register and added more style

--- src/components/UserLogIn.js
+++ src/components/UserLogIn.js
@@ -1,5 +1,6 @@
 import react from 'react';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import './UserLogIn.css';
 
@@ -9,18 +10,20 @@ const UserLogIn = () => {
             <Row className="justify-content-md-center"><Col md={5} xs={9}>
                 <h1 className="login-header mb-4">Log In</h1>
                 <Form>
-                    <Form.Group className="mb-4" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Group className="mb-5" controlId="Email">
+                        <Form.Control type="email" placeholder="Email" />
                     </Form.Group>
                 
-                    <Form.Group className="mb-5" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
+                    <Form.Group className="mb-5" controlId="Password">
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
 
-                    <Button type="submit" className="login-submit">Submit</Button>
+                    <Button type="submit" className="login-submit mb-5">Log In</Button>
                 </Form>
+                <p>Not yet registered?</p>
+                <Link to="/signup">
+                    <Button className="login-register">Register Now</Button>
+                </Link>
             </Col></Row>
         </Container>
     )
