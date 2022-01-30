f03f166751f5d38867a038c3540ac5fb36f8b825
cesarsnina <cesar.nina05@gmail.com>
2022-01-30 23:01:30 +0000

    added a model for the dogs

---
+++ server/model/Dog.js
@@ -0,0 +1,15 @@
+const { db, DataTypes, Model } = require('../db');
+
+class Dog extends Model {}
+
+Dog.init({
+    name: DataTypes.STRING,
+    breed: DataTypes.STRING,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING,
+},{
+    sequelize: db,
+    timestamps: false,
+})
+
+module.exports = { Dog };
