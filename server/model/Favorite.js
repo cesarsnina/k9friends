7244d88d87abe4d7193a7adf3bbee07a2f517e0c
cesarsnina <cesar.nina05@gmail.com>
2022-02-01 21:30:23 +0000

    created a model called Favorite

---
+++ server/model/Favorite.js
@@ -0,0 +1,28 @@
+const { db, DataTypes, Model } = require('../db');
+const { User } = require('./User');
+const { Dog } = require('./Dog');
+
+class Favorite extends Model {}
+
+Favorite.init({
+    UserId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    DogId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Dog,
+            key: 'id'
+        }
+    }
+},
+{
+    sequelize: db,
+    timestamps: false,
+})
+
+module.exports = Favorite;
